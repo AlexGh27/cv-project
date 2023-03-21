64bbfcc673b035ecc866bcc45cdfb91df57d2945
AlexGh27 <alexghioca27@gmail.com>
2023-03-21 14:51:14 +0200

    Updates

---
+++ precache-manifest.fcbc2124d957df0e8e8964ea86502065.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fee51422c7a53a2cf67",
+    "url": "/cv-project/static/css/main.95c144e9.chunk.css"
+  },
+  {
+    "revision": "5fee51422c7a53a2cf67",
+    "url": "/cv-project/static/js/main.5fee5142.chunk.js"
+  },
+  {
+    "revision": "022280761d4275f8b4b5",
+    "url": "/cv-project/static/js/1.02228076.chunk.js"
+  },
+  {
+    "revision": "191a181b6a79c31ed82c",
+    "url": "/cv-project/static/js/runtime~main.191a181b.js"
+  },
+  {
+    "revision": "13947a96a2909293270dcc0e92efef3a",
+    "url": "/cv-project/index.html"
+  }
+];
