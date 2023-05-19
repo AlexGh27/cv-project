a4099238153fcb1a4969ebd6f7699f4fd3088c08
AlexGh27 <alexghioca27@gmail.com>
2023-05-19 15:39:39 +0300

    Adding state to all the components

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,66 @@
-import React, {Component} from "react";
-import Header from './Header';
+import React, { Component } from 'react';
 import General from './General';
 import Educational from './Educational';
 import Experience from './Experience';
 import Overview from './Overview';
+import Header from './Header';
 import Footer from './Footer';
 
+
 export default class App extends Component {
-  constructor() {
-    super();
-
+  constructor(props) {
+    super(props);
     this.state = {
-
-    }
+      general: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        city: '',
+        description: ''
+      },
+      education: [],
+      workExperience: []
+    };
   }
 
+  handleGeneralInfoChange = (fieldName, value) => {
+    this.setState(prevState => ({
+      generalInfo: { ...prevState.generalInfo, [fieldName]: value }
+    }));
+  };
+
+  handleAddEducational = (education) => {
+    this.setState(prevState => ({
+      education: [...prevState.education, education]
+    }));
+  };
+
+  handleAddExperience = (workExperience) => {
+    this.setState(prevState => ({
+      workExperience: [...prevState.workExperience, workExperience]
+    }));
+  };
+
   render() {
-    return(
-      <div className="app">
+    const { general, education, experience } = this.state;
 
-         <Header/>
-
-         <div className="left-panel">
-           <General/>
-
-           <Educational/>
-
-           <Experience/>
-         </div>
-
-         <div className="right-panel">
-            <Overview/>
-         </div>
-
-         <Footer/>
-
+    return (
+      <div>
+        <Header/>
+        <General
+          general={general}
+          onGeneralInfoChange={this.handleGeneralInfoChange}
+        />
+        <Educational onAddEducation={this.handleAddEducation} />
+        <Experience onAddWorkExperience={this.handleAddWorkExperience} />
+        <Overview
+          generalInfo={this.state.generalInfo}
+          education={education}
+          experience={experience}
+        />
+        <Footer/>
       </div>
-      
-    )
+    );
   }
 }
--- src/components/General.js
+++ src/components/General.js
@@ -1,30 +1,67 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
 
 export default class General extends Component {
+  handleFieldChange = (e) => {
+    const fieldName = e.target.name;
+    const value = e.target.value;
+    this.props.onGeneralInfoChange(fieldName, value);
+  };
 
-    render() {
-        return(
-            <div className="general">
-                <div className="field-title">General Information</div>
-                <form className="form">
+  render() {
+    const { general } = this.props;
 
-                    
-                        <input id="firstName" type="text" placeholder="First Name" required/>
-                    
-                        <input id="lastName" type="text" placeholder="Last Name" required/>
-                    
-                    
-                    
-                        <input id="email" type="email" placeholder="Email Address"/>
+    return (
+      <div className="general">
 
-                        <input id="tel" type="text" placeholder="Phone Number"/>
-                   
-                        <input id="city" type="text" placeholder="City"/>
+        <div className="field-title">General Information</div>
+        <form className="form">
+            <input
+                type="text"
+                id="firstName"          
+                name="firstName"
+                placeholder="First Name"
+                value={general.firstName || ''}
+                onChange={this.handleFieldChange}
+            />
 
-                        <input id="summary" type="text" placeholder="Short Description"/>
+            <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                placeholder="Last Name"
+                value={general.lastName}
+                onChange={this.handleFieldChange}
+            />
 
-                </form>
-            </div>
-        )
-    }
+            <input
+                type="email"
+                id="email"          
+                name="email"          
+                placeholder="Email Address"
+                value={general.email}
+                onChange={this.handleFieldChange}
+            />
+
+            <input
+                type="text"
+                id="tel"
+                name="tel"
+                placeholder="Phone Number"
+                value={general.tel}
+                onChange={this.handleFieldChange}
+            />
+
+            <input
+                type="text"
+                id="summary"
+                name="summary"
+                placeholder="Short Description"
+                value={general.summary}
+                onChange={this.handleFieldChange}
+            />
+            </form>
+        
+      </div>
+    );
+  }
 }
