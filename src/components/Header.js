97e20bfb016c17c49a5ff492f99a6aef3bc4ab14
AlexGh27 <alexghioca27@gmail.com>
2023-06-09 16:16:54 +0300

    Changed header fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,15 @@
 import React, {Component} from "react";
 
-
 export default class Header extends Component {
 
+
     render() {
+        
         return(
-            <div className="header">
-                <div className = "title">
-                    
-                </div>
-                <img src="../assets/github.png" alt="Github logo" />
-                <div className = "project">
-                    AlexGh27
+            <div className="header">  
+               <div className="github">My github</div>      
+               <div className = "project">
+                     Created by AlexGh27
                 </div>
 
             </div>
